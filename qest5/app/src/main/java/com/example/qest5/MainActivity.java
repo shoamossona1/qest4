4d16af4ed79b583ed25bc11c155a13c1bb51bf7f
shoamossona1 <shoamyhv@gmail.com>
2025-12-22 20:19:40 +0200

    1

---
+++ qest5/app/src/main/java/com/example/qest5/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.qest5;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class qest5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        }
+        }
+    }
+public void go (View view){
+    string click= ("Oh, yea, I’ve been clicked!");
+    Button.setText(cl);
+
+}
