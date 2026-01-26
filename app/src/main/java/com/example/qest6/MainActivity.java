db7f43f6bc4da396b5c7a6e5a72748444a227215
shoamossona1 <shoamyhv@gmail.com>
2026-01-26 22:39:49 +0200

    qest 6.1

---
+++ app/src/main/java/com/example/qest6/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.qest6;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView Tv;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Tv = findViewById(R.id.Tv);
+
+
+
+
+
+    }
+
+
+    public void go(View view) {
+        Tv.setText("Oh, yea, I’ve been clicked!");
+
+        }
+
+
+        }
+
