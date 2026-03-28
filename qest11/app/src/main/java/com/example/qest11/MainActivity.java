e1b06ce3f6ec562ab89e99b06cfd0c45d47cc57d
shoamossona1 <shoamyhv@gmail.com>
2026-03-28 22:21:26 +0300

    qest 11 3

--- qest11/app/src/main/java/com/example/qest11/MainActivity.java
+++ qest11/app/src/main/java/com/example/qest11/MainActivity.java
@@ -1,38 +1,44 @@
 package com.example.qest11;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
     WebView wV;
+    Button but;
+    EditText eT;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        private class MyWebViewClient extends WebViewClient {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        }
 
-        WebView wV = findViewById(R.id.WebView);
+        eT = findViewById(R.id.EditText);
+        wV = findViewById(R.id.WebView);
+        but = findViewById(R.id.button);
+
+
 
         wV.getSettings().setJavaScriptEnabled(true);
-
-        string url = "https://www.google.com/";
-        wV.loadUrl(stringurl);
+        String stringurl = "https://www.walla.co.il/";
         wV.setWebViewClient(new WebViewClient());
+        wV.loadUrl(stringurl);
 
 
-        });
+
+        }
+    public void go(View view) {
+        String url = eT.getText().toString();
+        wV.setWebViewClient(new WebViewClient());
+        wV.loadUrl(url);
     }
 }
